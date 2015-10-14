ff83ad1091b4b293e4a8e867a6916eba7dd2f5e3
kyleroden <kyleroden77@gmail.com>
2015-10-13 17:11:10 -0700

    part 1 of hw 2 done

--- hw2.py
+++ hw2.py
@@ -4,28 +4,23 @@ total_price = 0
 remaining_money = 0
 new_item_price = 0
 
-print('Welcome to the store. Type "DONE" to exit the program.')
+print('Welcome to the store. Type "ALL DONE" to exit the program.')
 
-budget = int(input("What is your budget?"))
-
-
+budget = float(input("What is your budget? "))
 
 while total_price < budget:
-    new_item_price = input("What's the price of your item?")
+    new_item_price = input("What's the price of your item? ")
       
-    if new_item_price == 'DONE':
+    if new_item_price == 'ALL DONE':
         break
     
-    grocery_list.append(int(new_item_price))
+    grocery_list.append(float(new_item_price))
     total_price = sum(grocery_list)
     remaining_money = budget - total_price
-    print("Added that price to the list. It now has " + str(len(grocery_list)) + " items.")
+    print("Added that price to the list. The list now has " + str(len(grocery_list)) + " items.")
     print('The total is now $' + str(total_price) + ' .')
     print('You have $' + str(remaining_money) + ' remaining.')
     continue
-    
-while str(new_item_price) == 'DONE':
-    break
-    
-print('Thank you for shopping at the store, goodbye.')                     
+
+print('Thank you for shopping at the store. You are $' + str(total_price - budget) + " OVER budget.")                     
     
